d63582440e3130b1046cbe163f8e72af3a8f8996
arfazkhan <47222474+arfazkhan@users.noreply.github.com>
2022-11-10 06:42:44 +0530

    Update countdown-custom.js

--- Exhibiz HTML/js/countdown-custom.js
+++ Exhibiz HTML/js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 03, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2022, 11, 10, 00)}); // year, month, date, hour
         });
 });		
 
